5c10695e8f7fb61aac64fe5515f4a633f929366d
magdkudama <magdkudama@gmail.com>
2014-11-30 23:09:46 +0100

    Initial commit, with setuptools + license

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='django-js-routing',
+    version='1.0.0',
+    url='https://github.com/magdkudama/django-js-routing',
+    author='Magd Kudama',
+    author_email='magdkudama@gmail.com',
+    license='MIT',
+    packages=['django', 'routing', 'js'],
+    include_package_data=True,
+    description='Expose your Django routes to JavaScript',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+    ],
+)
